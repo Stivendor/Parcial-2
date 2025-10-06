1a851257c56beefe06d6f3b42087f7e6437ce9e5
Angel_7195 <angelgutierrez1132998@correo.itm.edu.co>
2025-10-06 12:49:22 -0500

    quitando routers de grupo y periodo

--- menu_prueba.py
+++ menu_prueba.py
@@ -1,6 +1,6 @@
 import uvicorn
 from fastapi import FastAPI
-from apis import usuario, estudiante, auditoria, profesor, materia, nota, grupo, periodo, persona # importa tus routers
+from apis import usuario, estudiante, auditoria, profesor, materia, nota, persona # importa tus routers
 
 app = FastAPI(title="API Proyecto Estructura de Datos")
 
@@ -11,8 +11,6 @@ app.include_router(auditoria.router, prefix="/auditorias", tags=["Auditorias"])
 app.include_router(profesor.router, prefix="/profesores", tags=["Profesores"])
 app.include_router(materia.router, prefix="/materias", tags=["Materias"])
 app.include_router(nota.router, prefix="/notas", tags=["Notas"])
-app.include_router(grupo.router, prefix="/grupos", tags=["Grupos"])
-app.include_router(periodo.router, prefix="/periodos", tags=["Periodo"])
 app.include_router(persona.router, prefix="/personas", tags=["Personas"])
 
 if __name__ == "__main__":
